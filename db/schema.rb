f4ca217c609d2397ef6301b76b725d8678b6748a
Chris920 <cdtded1212@gmail.com>
2015-07-07 00:13:05 -0400

    added reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707033548) do
+ActiveRecord::Schema.define(version: 20150707040134) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "schools", force: :cascade do |t|
