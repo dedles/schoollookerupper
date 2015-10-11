f799960bf757f1f1ae4c8d9af02549d4cb649102
Chris920 <cdtded1212@gmail.com>
2015-10-11 02:25:04 -0400

    added confirmable and lockable to devise and set up mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,18 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # required for devise; remember to change localhost to actual location of host
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+	config.action_mailer.default_url_options = { host: 'schoollookerupper.herokuapp.com' }
+
+	ActionMailer::Base.smtp_settings = {
+	    :address        => 'smtp.sendgrid.net',
+	    :port           => '587',
+	    :authentication => :plain,
+	    :user_name      => ENV['SENDGRID_USERNAME'],
+	    :password       => ENV['SENDGRID_PASSWORD'],
+	    :domain         => 'heroku.com',
+	    :enable_starttls_auto => true
+	}
+
 end
