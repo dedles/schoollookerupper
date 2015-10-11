4ef676b6cdd30b5af7130272331523308f050ca9
Chris920 <cdtded1212@gmail.com>
2015-10-11 16:22:38 -0400

    add favicon, disable email editability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,31 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) << :first_name
+  #   devise_parameter_sanitizer.for(:sign_up) << :last_name
+  #   devise_parameter_sanitizer.for(:account_update) << :first_name
+  #   devise_parameter_sanitizer.for(:account_update) << :last_name
+  # end
+
+
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name
-    devise_parameter_sanitizer.for(:sign_up) << :last_name
-    devise_parameter_sanitizer.for(:account_update) << :first_name
-    devise_parameter_sanitizer.for(:account_update) << :last_name
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
+      :email,
+      :first_name,
+      :last_name,
+      :password,
+      :password_confirmation)
+    }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+      :first_name,
+      :last_name,
+      :password,
+      :password_confirmation,
+      :current_password
+      )
+    }
   end
 
 end
